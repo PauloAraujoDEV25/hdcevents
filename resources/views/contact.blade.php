5f2de1287c44f841aa3912b6cb5ae405e6060845
paulo <pauloaraujoDEV25@gmail.com>
2024-01-01 10:44:51 -0300

    Initial commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('title','HDC EVENTS')
+
+@section('content')
+
+<h1>Está é a página de contato</h1>
+<a href="/">Voltar para a Home</a>
+
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    
+    <!-- Fonte do Google -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter&family=Roboto" rel="stylesheet">
+
+    <!-- Css BootStrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+
+    <!-- Css da Aplicação -->
+    <link rel="stylesheet" href="/css/styles.css">
+
+    <!-- JavaScript da Aplicação -->
+    <script src="/js/script.js"></script>
+    
+</head>
+
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="collapse navbar-collapse" id="navbar">
+                <a href="/" class="navbar-brand">
+                    <img src="/img/hdcevents_logo.svg" alt="logo">
+                </a>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Eventos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/events/create" class="nav-link">Criar Eventos</a></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Cadastrar</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+                @if(session('msg'))
+                <p class="msg">{{ session('msg') }}</p>
+                @endif
+                @yield('content')
+            </div>
+        </div>
+    </main>
+    
+    <footer>
+    <p>HDC EVENTS &copy; 2023</p>
+    </footer>
+</body>
+
+</html>

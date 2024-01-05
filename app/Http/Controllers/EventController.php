a1bc55f84b73e41c9dca2d204e9bb4802893924e
paulo <pauloaraujoDEV25@gmail.com>
2024-01-04 21:07:47 -0300

    atualizando  a criação de eventos
    adicionando mais funções ao formulário de criação de evento.

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -27,6 +27,7 @@ class EventController extends Controller
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+        $event->items = $request->items;
 
 
         // Image Upload
@@ -49,6 +50,14 @@ class EventController extends Controller
         return redirect('/')->with('msg','Evento Criado com sucesso!') ;
      }
 
+     public function show($id) {
+
+        $event = Event::findOrFail($id);
+
+        return view('events.show', ['event' => $event]);
+
+     }
 }
+
 
 
